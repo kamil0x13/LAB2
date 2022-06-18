6c2204b86cef1f9f5267efb7f14cfb40a783ff88
Kamil Pietrzak <pietrzakkamil013@gmail.com>
2022-06-18 06:16:55 +0200

    Wyświetlanie tekstu po wciśnięciu przycisku

--- src/main/java/com/example/lab2/HelloController.java
+++ src/main/java/com/example/lab2/HelloController.java
@@ -3,12 +3,12 @@ package com.example.lab2;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class    HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Wciścnięt przycisk");
     }
 }
